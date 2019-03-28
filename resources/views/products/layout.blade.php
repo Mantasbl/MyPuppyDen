fb0a677bec013bf8b56b1ee318fd6b5ec169aace
Mantasbl <Mantas.baltakis@gmail.com>
2019-03-28 10:18:59 +0000

    Added Products CRUD views, Browser-Sync

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <title>MyPuppyDen</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
 
